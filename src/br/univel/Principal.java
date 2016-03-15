ee0f915390af3adb79c8316a63388bfc0bc0e0b1
Matheus Zandoná <zan_matheus@yahoo.com.br>
2016-03-15 20:19:33 -0300

    Alteraçao a pedido do cliente.

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -9,7 +9,7 @@ package br.univel;
 public class Principal {
 
 	public static void main(String [] args){
-		System.out.println("Olá Mundo !");
-		System.out.println("Teste !");
+		System.out.println("Hello World !");
+		System.out.println("It's we !");
 	}
 }
