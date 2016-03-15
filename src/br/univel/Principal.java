ef3d774fa0ba7971534fdb5f54d510cc187905e8
Matheus Zandoná <zan_matheus@yahoo.com.br>
2016-03-15 20:10:26 -0300

    Usando OO.

--- src/br/univel/Principal.java
+++ src/br/univel/Principal.java
@@ -9,7 +9,6 @@ package br.univel;
 public class Principal {
 
 	public static void main(String [] args){
-		System.out.println("Olá Mundo !");
-		System.out.println("Teste !");
+		new Message().hello();
 	}
 }
